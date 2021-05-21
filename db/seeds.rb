dc963d9aa650c163d89675a4683e99573828e961
climb719 <mary.oldweiler@gmail.com>
2021-05-21 15:37:17 -0400

    commit tree 96b1f091b6a429c5706070e619eb52cc3ffc1e05

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+students = Student.create([{first_name: "Daenerys", last_name: "Targaryen"},{first_name: "Arya", last_name: "Stark"}, {first_name: "Sansa", last_name: "Stark"}])
+school_classes = SchoolClass.create([{title: "Computer Science", room_number: 5}, {title: "Software Engineering", room_number: 7}, {title: "Hacking 101", room_number: 11}])
